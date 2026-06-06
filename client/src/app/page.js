d50470e0d31d1be9df563442c32feb2ffe1077b6
umayer10-AI <mdumayer10@gmail.com>
2026-06-06 12:26:26 +0600

    add

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,14 @@
 import Banner from '@/component/Banner';
+import HeroBanner from '@/component/HeroBanner';
+import HeroUISection from '@/component/HeroUISection';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       <Banner></Banner>
+      <HeroBanner></HeroBanner>
+      <HeroUISection></HeroUISection>
     </div>
   );
 };
