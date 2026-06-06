0976b10d791abf9821697acae9bb34c1f336d276
umayer10-AI <mdumayer10@gmail.com>
2026-06-06 19:38:40 +0600

    add

--- client/src/app/(dashboardLayout)/dashboard/page.jsx
+++ client/src/app/(dashboardLayout)/dashboard/page.jsx
@@ -2,9 +2,24 @@ import OrganizerDashboard from '@/component/organizerDashbaord/OrganizerDashboar
 import React from 'react';
 
 const page = () => {
+
+    const role = 'attendee'
+    let overview;
+
+    if(role==='organizer'){
+        overview = <div>
+                        <OrganizerDashboard></OrganizerDashboard>
+                    </div>
+    }
+    if(role==='attendee'){
+        overview = <div>
+                        Hello organizer
+                    </div>
+    }
+
     return (
         <div className='max-w-[90%] ml-10'>
-            <OrganizerDashboard></OrganizerDashboard>
+            {overview}
         </div>
     );
 };
